a677986f45757cc55a75b0e1be4b45747139e7c1
LeixB <abone9999@gmail.com>
2022-05-23 19:45:47 +0200

    Add sample jena program

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,26 @@
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.vocabulary.RDFS;
+
 class Main {
+
+    static String baseURI = "https://localhost/papers";
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // some definitions
+        final String personURI    =  baseURI + "/person";
+        final String fullName     = "John Smith";
+
+        // create an empty Model
+        Model model = ModelFactory.createDefaultModel();
+
+        // create the resource
+        model.createResource(personURI + "#JohnSmith").
+            addProperty(RDFS.label, fullName);
+
+        // write the model in RDF/XML syntax to standard output
+        model.write(System.out, "RDF/XML");
     }
 }
